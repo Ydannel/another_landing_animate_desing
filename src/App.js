b62704b7aad3688bdf76a0b9748f4fc631979df7
Ydannel <44483665+Ydannel@users.noreply.github.com>
2021-08-25 14:46:06 -0400

    add new pages
    just finish navbar and the styles for that navigation
    
    modify packa.json
    
    change the title at the index.html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Navbar from './components/Navbar'
 
+//#881600 amarillo
+//#4A148C purpura oscuro
+//#E65100 naranja profundo
 function App() {
   return (
     <div className="App">
-       
+       <Navbar/>
     </div>
   );
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,34 @@
+import {Nav, NavbarContainer, LogoNav, NavMenu, NavItem, NavLinks, MobileIcon, Navbtn, NavbtnLink} from './styles'
+import {FaBars} from 'react-icons/fa'
 const Navbar = () => {
     return ( 
-        <div>
-            
-        </div>
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <LogoNav to='/'>Carnibal fantasy</LogoNav>
+                    <MobileIcon>
+                        <FaBars/>
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='discover'>Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='services'>Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='singup'>Sing Up</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                   <Navbtn>
+                         <NavbtnLink to='/singin'>Sing In</NavbtnLink>
+                    </Navbtn> 
+                </NavbarContainer>
+            </Nav>
+        </>
      );
 }
  
