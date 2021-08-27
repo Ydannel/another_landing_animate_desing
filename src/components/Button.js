cd4ff933c219c14d6b963e2bd2cc9e08487a142f
Ydannel <44483665+Ydannel@users.noreply.github.com>
2021-08-27 11:05:44 -0400

    added button styles
    .

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,24 @@
 import styled from 'styled-components';
+import {Link} from 'react-scroll'
 
-import {Link as LinkR} from 'react-router-dom'
-import {Link as LinkS} from 'react-scroll'
+export const Button = styled(Link)`
+    border-radius: 50px;
+    background: ${({primary})=> (primary ? '#E65100' : '#881600')};
+    white-space: nowrap;
+    padding: ${({big})=> (big ? '14px 48px' : '12px 30px')};
+    color: '#fffffe';
+    font-size: ${({fontBig})=> (fontBig ? '20px' : '16px')};
+    outline: none;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.2s ease-in-out;
 
-export const Button = styled()``
+    &:hover{
+        transition: all 0.2s ease-in-out;
+        background: #4A148C;
+        color: #fffffe;
+    }
+`
