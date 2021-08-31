417fdd255925179a898f7c1b7cf57ee3e3ee803a
Ydannel <44483665+Ydannel@users.noreply.github.com>
2021-08-31 10:43:13 -0400

    .
    .

---
+++ src/components/Info/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container =styled.div``
+export const Wrapper =styled.div``
+export const InfoRow =styled.div``
+export const Column1 =styled.div``
+export const Text =styled.div``
+export const TopLine =styled.div``
+export const Heading =styled.div``
+export const Subtitle =styled.div``
+export const BtnWrap =styled.div``
